1bb769ede75a06c90866ff90f38d225057d0ccc0
Frankie B <frankieraybrown@gmail.com>
2021-06-01 22:44:45 +0100

    Added main files

---
+++ annoy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/play '/var/www/html/succ/filename.wav'
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>no</title>
+</head>
+
+<body>
+	<h1>oh shit</h1>
+
+<?php
+$audio = shell_exec('sh annoy.sh');
+echo $audio;
+?>
+
+	<a href=".">oh no</a>
+</body>
+</html>
+
